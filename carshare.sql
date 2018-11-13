c4b51fd089dcc62e801dbec2f65fa6eb1eaad4fb
Darren Chan <darrennchan8@gmail.com>
2018-11-13 16:32:42 -0500

    Add parking lot

--- carshare.sql
+++ carshare.sql
@@ -1,15 +1,24 @@
+CREATE TABLE parking_lot (
+    lot_id INT AUTO_INCREMENT PRIMARY KEY,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state CHAR(2) NOT NULL,
+    zip_code INT(5) NOT NULL,
+    capacity INT UNSIGNED
+);
+
 create table TRIP_DETAILS (
-    reservation decimal PRIMARY KEY,
-    reservationStart decimal,
-    reservationEnd decimal,
-    actualStart decimal,
-    actualEnd decimal,
+    reservation INT AUTO_INCREMENT PRIMARY KEY,
+    reservation_start decimal,
+    reservation_end decimal,
+    actual_start decimal,
+    actual_end decimal,
     rate decimal);
 
 create table ACCOUNT (
     email varchar(50) PRIMARY KEY,
     passHash varchar(100),
-    FirstName varchar (20),
+    FirstName varchar(20),
     LastName varchar(50),
     address varchar(60),
     city char(40),

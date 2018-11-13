d134b9f737ea5aa797f92051740a85035c07b522
Daniel L Rodriguez <rodriguezdl@egr-maclab-35.local>
2018-11-13 16:16:37 -0500

    split table creation from sample data insert

--- carshare-sample.sql
+++ carshare-sample.sql
@@ -2,39 +2,39 @@
 insert into TRIP_DETAILS (reservation, reservationStart, reservationEnd, actualStart, actualEnd, rate)
     values()
 
--- Insert Sample Data into account
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+-- Insert sample data into ACCOUNT
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('rodriguezdl@vcu.edu', '$2a$12$0TBPmJfOif1q4m9DFCQPVOzw4xkQsqWNiiF9dIHGH3Wepz34a3R6C', 'Daniel', 'Rodriguez',
     '340 College Avenue', 'Commack', 'New York', 11725, 2025550149, 1538524728801);  
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('chand3@vcu.edu', '$2a$12$PSkGhrXH24plG5lUoWTKAOFLmOXU85TM8.SKqgW1LDZGZSCmTQBdi', 'Darren', 'Chan',
     '8135 Sutor Drive ', 'Northville', 'Michigan', 48167, 2127378040, 1538963776422); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('dhinganiv@vcu.edu', '$2a$12$XljWZ8UuEJ70k2D1NWbRRu1YKmKFoDU00dErWRJIEfJ.VEEO2ulTS', 'Vasu', 'Dhingani',
     '81 Pilgrim Ave.', 'Chester', 'Pennsylvania', 19013, 4064287501, 1538864010510); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('martinkl@gmail.com', '$2a$12$Jk3mzqgNTR9S/D4giGs5wOZKaXcK3KD0X/mRsrxuHNKlxQPlTOKc6', 'King', 'Martin',
     '31 South Street', 'New Albany', 'Indiana', 471250, 5054610705, 1538998002062); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('s2dmduke@vcu.edu', '$2a$12$hflgY5ItHNV86skL1srfuOsnEHiNl496wkkKakCx1P2LDgCBDEYem', 'Debra', 'Duke',
     '506 East Armstrong Avenue', 'Plymouth', 'Massachusetts', 02360, 205-967-8509, 1538567650524); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('plooer@aol.net', '$2a$12$Vk7AD572qOvOl/sENgJBVegf6v8tFcwbKfYYRYjfaV/KFJ1HgZJHq', 'Darren', 'Chan',
     '8135 Sutor Drive ', 'Northville', 'Michigan', 48167, 2127378040, 1538963776422); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('chand3@vcu.edu', '$2a$12$PSkGhrXH24plG5lUoWTKAOFLmOXU85TM8.SKqgW1LDZGZSCmTQBdi', 'Ere', 'Ploo',
     '160 8th St.', 'Caldwell', 'New Jersey', 07006, 2028216794, 1539049922050); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('Patelviyat@gmail.com', '$2y$12$Z46FuqPRt.nm7S3NY.BzMO.NQh/sINCO.SQUHw7tXVg.zVE9QLMo.', 'Viyat', 'Patel',
     '4820 Spring Street ', 'Effingham', 'Illinois', 62401, 8898790930, 1539117108806); 
 
-insert into account(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
+insert into ACCOUNT(email, passHash, FirstName, LastName, address, city, state, zipCode, phoneNum, creationDate) 
     values ('Jmh@gmail.com', '$2y$12$Z46FuqPRt.nm7S3NY.BzMO.NQh/sINCO.SQUHw7tXVg.zVE9QLMo.', 'Hakiziman', 'Jean-Marie',
     '2763 Payne Street', 'Davenport', 'Virginia', 24239, 2768591677, 1539096222812); 
--- carshare.sql
+++ carshare.sql
@@ -1,3 +1,4 @@
 create table TRIP_DETAILS (reservation decimal PRIMARY KEY, reservationStart decimal, reservationEnd decimal, actualStart decimal, actualEnd decimal, rate decimal);
 
-create table account(email varchar(50) PRIMARY KEY, passHash varchar(100), FirstName varchar (20), LastName varchar(50), address varchar(60), city char(40), state char(40), zipCode decimal, phoneNum decimal, creationDate bigint);
+create table ACCOUNT(email varchar(50) PRIMARY KEY, passHash varchar(100), FirstName varchar (20), LastName varchar(50), address varchar(60), city char(40), state char(40), zipCode decimal, phoneNum decimal, creationDate bigint);
+

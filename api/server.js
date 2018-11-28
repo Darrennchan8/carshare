67ebac32be5952bcb05c2d4acbd78a92a4a7fbc7
Darren Chan <darrennchan8@gmail.com>
2018-11-27 22:48:56 -0500

    Add working login with bcrypt

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -29,3 +30,37 @@ db.connect(err =>
         console.error('Unable to connect to database', err) :
         console.log('Connected to mysql!'));
 
+const query = function(str, ...params) {
+  return new Promise((resolve, reject) => {
+    db.query(str, params, (error, results, fields) => {
+      if (error) {
+        console.error({
+          invocation: [str, ...params],
+          error
+        });
+      }
+      resolve(results);
+    });
+  });
+}
+
+router.get('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send('Bad Request');
+    return;
+  }
+  const expected = (await query('SELECT * FROM account WHERE email_address=?', email))[0] || {};
+  const actualHash = await bcrypt.hash(password, expected.salt || '');
+  console.log(expected, actualHash);
+  if (expected && expected.password_hash == actualHash) {
+    res.json({
+      success: true
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'Incorrect username or password.'
+    });
+  }
+});
